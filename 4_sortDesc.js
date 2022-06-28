7c97eccde417e0d9c64b5d85aef09845bc6ce029
Runidan <rudan@zlatoust.en.cx>
2022-06-28 11:12:36 +0500

    fix: sortDesc with sort method

--- 4_sortDesc.js
+++ 4_sortDesc.js
@@ -2,17 +2,8 @@
 Реализовать функцию для сортировки массива чисел по убыванию
 */
 
-function sortDesc(arr_num) {
-  let i = 0
-  while (i < arr_num.length){
-    if(arr_num[i] < arr_num[i + 1]){
-      [arr_num[i], arr_num[i + 1]] = [arr_num[i + 1], arr_num[i]];
-      i = 0;
-      continue;
-    }
-    i++;
-  }
-  return arr_num;
+function sortDesc(arr) {
+  return arr.sort(function(a, b) { return b - a; });
 }
 
 const numbers = [-20, -10, 0, 10, 20, 30];
