ea09286a687483348f1170ef21417eeef0ce7ad4
Runidan <45917212+Runidan@users.noreply.github.com>
2022-07-13 17:54:40 +0500

    Update Lesson4/1.js
    Co-authored-by: Владислав Пономарёв <Git@PonomareVlad.ru>

--- Lesson4/1.js
+++ Lesson4/1.js
@@ -3,7 +3,7 @@
 чтобы получить возможность выполнять случайную сортировку для любого массива
 */
 
-Array.prototype.shuffle = function(){return this.sort(function() { return Math.random() - Math.random(); })};
+Array.prototype.shuffle = function(){return [...this].sort(() => Math.random() - 0.5)};
 
 // let arr = [1, 2, 3, 4, 5, 6];
 // arr.shuffle();
