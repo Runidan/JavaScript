7433cd7ad99a0bc757c25441cc2b6dcf3805a3be
Runidan <rudan@zlatoust.en.cx>
2022-07-13 12:56:25 +0500

    feat: task 2 is done

---
+++ Lesson4/2.js
@@ -0,0 +1,22 @@
+/*
+С помощью прототипа реализовать структуру "Квадрат" которая будет наследоваться от структуры "Прямоугольник",
+ должна быть возможность задавать размеры обеим фигурам и получать их площадь и периметр
+*/
+
+function Rectangle(size1, size2) {
+  this.size1 = size1;
+  this.size2 = size2;
+
+  this.getArea = function(){
+    return this.size1 * this.size2;
+  } 
+
+  this.getPerimetr = function () {
+    return ( this.size1 + this.size2 ) * 2;
+  }
+}
+
+function Square(size) {
+  this.size1 = size;
+  this.size2 = size;
+}
