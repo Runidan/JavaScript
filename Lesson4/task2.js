a3ff403a30c1c2e6249cf8654dafd003a6768328
Runidan <rudan@zlatoust.en.cx>
2022-07-18 12:57:18 +0500

    fix: after rewriting

--- Lesson4/task2.js
+++ Lesson4/task2.js
@@ -20,8 +20,46 @@
 
 Верфи и корабли должны создаваться с помощью функций-конструкторов.
 */
+
+function ShipRope(typeShip) {
+ 
+  // Строить корабли - только своего типа
+  this.build_ship = function(a, b) {
+    return new typeShip(a, b);
+  }
+
+  // Ремонтировать корабли - Должен проверяться тип корабля, работать только с кораблями своего типа
+  this.repair_ship = function(ship) {
+    if (ship.constructor === typeShip){
+      console.log('Восстанавливаем здоровье');
+      ship.health = 100;
+    }
+  }
+
+  // Обменивать старый корабль на новый - Можно обменивать только корабли того же типа
+  this.change_ship = function(ship, a, b) {
+    if (ship.constructor === typeShip){
+      return this.build_ship(a, b);
+    }
+  }
+
+  // Перекрашивать корабли - Можно красить любые корабли
+  this.recolor_ship = function(ship, color) {
+    ship.color = color;
+  }
+  
+}
+
+function PowerShipRope() {
+  return new ShipRope(Powerboat);
+}
+
+function SailShipRope() {
+  return new ShipRope(Powerboat);
+}
+
 function Ship() {
-  this.color = 20;
+  this.color = 'white';
   this.health = 100;
 }
 
@@ -31,12 +69,6 @@ function Powerboat(power, hull) {
   this.hull = hull;
 }
 
-Powerboat.prototype = Object.create(Ship.prototype);
-
-Object.defineProperty(Powerboat.prototype, 'constructor', {
-  value: Powerboat,
-  enumerable: false, 
-  writable: true });
 
 function SailingShip(masts, sailArea) {
   Ship.call(this)
@@ -44,88 +76,14 @@ function SailingShip(masts, sailArea) {
   this.sailArea = sailArea;
 }
 
-SailingShip.prototype = new Ship();
 
-Object.defineProperty(SailingShip.prototype, 'constructor', {
-  value: SailingShip,
-  enumerable: false, 
-  writable: true });
-
-function ShipRope(type) {
- 
-  // Перекрашивать корабли - Можно красить любые корабли
-  this.recolor_ship = function(ship, color) {
-    ship.color = color;
-  }
-}
-
-
-function PowerShipRope() {
-  // Строить корабли - только своего типа
-  this.build_ship = function(a, b) {
-    return new Powerboat(a, b);
-  }
-
-  // Ремонтировать корабли - Должен проверяться тип корабля, работать только с кораблями своего типа
-  this.repair_ship = function(ship) {
-    if (ship.constructor === Powerboat){
-      console.log('Восстанавливаем здоровье');
-      ship.health = 100;
-    }
-  }
-
-    // Обменивать старый корабль на новый - Можно обменивать только корабли того же типа
-    this.change_ship = function(ship, a, b) {
-    if (ship.constructor === Powerboat){
-      return this.build_ship(a, b);
-    }
-  }
-}
-
-PowerShipRope.prototype = new ShipRope();
-
-Object.defineProperty(PowerShipRope.prototype, 'constructor', {
-  value: PowerShipRope,
-  enumerable: false, 
-  writable: true });
-
-function SailShipRope() {
-
-  // Строить корабли - только своего типа
-  this.build_ship = function(a, b) {
-    return new SailingShip(a, b);
-  }
-
-  // Ремонтировать корабли - Должен проверяться тип корабля, работать только с кораблями своего типа
-  this.repair_ship = function(ship) {
-    if (sh.constructor === SailingShip){
-      ship.health = 100;
-    }
-  }
-
-  // Обменивать старый корабль на новый - Можно обменивать только корабли того же типа
-  this.change_ship = function(ship, a, b) {
-    if (ship.constructor === SailingShip){
-      return this.build_ship(a, b);
-    }
-  }
-}
-
-SailShipRope.prototype = new ShipRope();
-
-Object.defineProperty(SailShipRope.prototype, 'constructor', {
-  value: SailShipRope,
-  enumerable: false, 
-  writable: true });
-
-let sh = new Powerboat(7, 3);
-console.log('color: ' + sh.color);
-console.log('здоровье ' + sh.health);
-let rope = new PowerShipRope();
-sh.health = 50;
-console.log('здоровье ' + sh.health);
-rope.repair_ship(sh);
-console.log('здоровье ' + sh.health);
-console.log('color: ' + sh.color);
-rope.recolor_ship(sh, 'green');
-console.log('color: ' + sh.color);
+// let rope = new PowerShipRope();
+// let sh = rope.build_ship(7, 3);
+// console.log(sh);
+// sh.health = 50;
+// console.log('здоровье ' + sh.health);
+// rope.repair_ship(sh);
+// console.log('здоровье ' + sh.health);
+// console.log('color: ' + sh.color);
+// rope.recolor_ship(sh, 'green');
+// console.log('color: ' + sh.color);
