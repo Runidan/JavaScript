1cad3fb5987b2609de63c74b3c31b144faf0ec89
Runidan <rudan@zlatoust.en.cx>
2022-07-20 16:44:23 +0500

    feat: task5 is done

--- Lesson5/game.js
+++ Lesson5/game.js
@@ -7,3 +7,29 @@
 Сколько ходов прошло с начала игры
 Всю историю ходов.
 */
+
+class Game{
+  constructor(){
+    this.status = 0;//0 - игра продолжается, 1 - победа белого, -1 - победа черного
+    this.deleted_piece = [];//для складирования объектов фигур
+    this.move_white = true;//true - ход белых, false - ход черных
+    this.count_move = 0; //счетчик ходов
+    this.history_move = []; //список ходов
+    this.chessboard = new ChessBoard(); //создаем доску
+    this.chessboard.start_position();  //расставляем фигуры
+  }
+
+  move(piece, target){
+    //проверяем target в массиве доступных piece.getAvalibleMove()
+    //проверям что между peice.place и target клетки свободны (кроме коня)
+    //если на клетке target фигура переносим в this.deleted_piece
+    //меняем знаячения в this.chessboard.board и в peice.place
+    //меняем  this.move_white на противоположное
+    //увеличивам this.count_move
+    //добавляем ход в this.history_move
+    //проверяем условия окончания игры (мат, пат), если да, меняем this.status
+  }
+
+  is_end(){}//проверка условия окончания партии
+
+}
--- Lesson5/pieces.js
+++ Lesson5/pieces.js
@@ -8,3 +8,53 @@ Cодержит свойство своего цвета
 
 Общие методы и свойства должны быть вынесены в родительский класс единый для всех фигур.
 */
+
+class Peice {
+  constructor(color, place){
+    this.white = Boolean(color);
+    this.place = place;
+  }
+
+  getAvalibleMove() {
+    //Метод переписать под каждую фигуру с учетом правил
+  }
+}
+
+class King extends Peice {
+  constructor(color, place){
+    super(color, place);
+  }
+}
+class Queen extends Peice {
+  constructor(color, place){
+    super(color, place);
+  }
+}
+
+class Bishop extends Peice {
+  constructor(color, place){
+    super(color, place);
+  }
+}
+
+class Knight extends Peice {
+  constructor(color, place){
+    super(color, place);
+  }
+}
+
+class Castle extends Peice {
+  constructor(color, place){
+    super(color, place);
+  }
+}
+
+class Pawn extends Peice {
+  constructor(color, place){
+    super(color, place);
+    this.can_change = true;
+  }
+
+  static peice_for_change = [Castle, Knight, Bishop, Queen]
+}
+
