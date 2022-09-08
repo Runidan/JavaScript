8d3a83052ddd866cd29d666bceb097885c42724e
Runidan <rudan@zlatoust.en.cx>
2022-09-08 20:52:19 +0500

    feat: added tests for 1-4 tasks

--- test.js
+++ test.js
@@ -41,7 +41,101 @@ describe("Test count_positive_numbers function", function() {
   });
 
   it('Выбрасывает исключение, если передать не массив', function() {
-    assert.throw(() => {count_positive_numbers('string')}, 'необходимо передать массив');
+    assert.throw(() => count_positive_numbers('string'), 'необходимо передать массив');
   });
 
 });
+
+// Тест функции для фильтрации массива чисел по убыванию
+describe('Тест функции фильтрации массива чисел по убыванию', function () {
+  let arr = sortDesc([-20, -10, 0, 10, 20, 30]);
+  
+  it('sortDesc является функцией', function() {
+    assert.isFunction(sortDesc);
+  });
+  
+  it('функция возвращает массив', function() {
+    assert.isArray(arr);
+  });
+  
+  it('Проверка работы функции', function() {
+    assert.deepEqual(arr, [30, 20, 10, 0, -10, -20]);
+  });
+
+  it('Тест с нулевым массивом', function() {
+    assert.equal(sortDesc([]), 0);
+  });
+
+  it('Выбрасывает исключение, если передать не массив', function() {
+    assert.throw(() => sortDesc('string'), 'arr.sort is not a function');
+  });
+
+});
+
+// Тест функции для фильтрации массива по длине слов
+describe('Тест функции для фильтрации массива по длине слов', function () {
+  const fruits = ['lime', 'orange', 'apple', 'banana', ''];
+
+  
+  it('filterByLength является функцией', function() {
+    assert.isFunction(filterByLength);
+  });
+
+  it('проверить работу фукции с корректными данными', function() {
+    assert.deepEqual(filterByLength(fruits, 0, 5), [ 'lime', 'apple', '' ]);
+  });
+
+  it('проверить работу фукции с данными тругих типов', function() {
+    assert.deepEqual(filterByLength(['lime', 'orange', 'apple', 'banana', true, false, 10], 0, 5), [ 'lime', 'apple' ]);
+  });
+
+  it('Выбрасывает исключение, если диапазон не задан числами', function() {
+    assert.throw(() => filterByLength([fruits], 'start', true), 'диапазон должен быть задан числами integer');
+  });
+
+});
+
+// Кэширование
+describe('Тест функции для фильтрации массива по длине слов', function () {
+  let calc = cache();
+  
+  it('cache является функцией', function() {
+    assert.isFunction(cache);
+  });
+
+  it('cache возвращает функцию', function() {
+    assert.isFunction(cache());
+  });
+
+  it('выбрасывает ошибку, если аргумент не является числом', function() {
+    assert.throw(() => calc(3, '10'), 'аргумент не является числом');
+  });
+
+  it('проверка работы с корректными данными', function() {
+    assert.deepEqual(calc(2, 10), { value: 1024, fromCache: false});
+    assert.deepEqual(calc(2, 10), { value: 1024, fromCache: true});
+  });  
+
+});
+
+// Реализуйте функцию MoneyBox
+describe('Тест функции MoneyBox', function () {
+  it('MoneyBox является функцией', function() {
+    assert.isFunction(MoneyBox);
+  });
+
+  const box = new MoneyBox();
+
+  it('метод getAmount существует', function() {
+    assert.exists(box.getAmount());
+  });
+
+  it('проверка работы с корректными данными', function() {
+    box.addCoin();
+    box.addCoin();
+    assert.equal(box.getAmount(), 2)
+  });
+});
+
+// Калькулятор
+// Система продажи билетов
